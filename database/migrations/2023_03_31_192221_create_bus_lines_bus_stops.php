a2ac12df6831b3dae27763cc64cad55e3c508455
MatijaBojanic <matija.bojanic@gmail.com>
2023-03-31 19:24:51 +0200

    Added bus_lines and bus_stops

---
+++ database/migrations/2023_03_31_192221_create_bus_lines_bus_stops.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('bus_lines_bus_stops', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bus_line_id')->constrained();
+            $table->foreignId('bus_stop_id')->constrained();
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('bus_lines_bus_stops');
+    }
+};
