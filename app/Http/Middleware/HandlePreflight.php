af07e33b0f78a1a2031402c15cd6050c02db5a52
MatijaBojanicC <matija.bojanic@codeus.me>
2023-06-24 13:45:49 +0200

    Preparation for gpt plugin

---
+++ app/Http/Middleware/HandlePreflight.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HandlePreflight
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 200)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/async/documentation', function () {
     return view('asyncapi');
 });
+
+Route::get('/.well-known/ai-plugin.json', function () {
+    return Storage::disk('public')->response('.well-known/ai-plugin.json');
+})->middleware(\App\Http\Middleware\HandlePreflight::class);
+
+Route::get('/.well-known/openapi.json', function () {
+    return Storage::disk('public')->response('.well-known/api-docs.json');
+});
+
+Route::get('logo.png', function () {
+    return Storage::disk('public')->response('logo.png');
+});
+
+
+Route::get('legal', function () {
+    return Storage::disk('public')->response('legal');
+});
